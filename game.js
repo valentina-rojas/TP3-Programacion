86a3cd6691eeef649e9b2ab829edc729915d989f
valentina-rojas <valentinarojas618@gmail.com>
2023-08-14 13:21:54 -0300

    mejoras

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 export default class Game extends Phaser.Scene {
   constructor() {
-
     super("game");
   }
 
@@ -8,6 +7,8 @@ export default class Game extends Phaser.Scene {
     this.puntos = 0;
     this.choques = 0;
     this.nivel = 1;
+    this.vidas = 5;
+    this.toqueBorde = false;
   }
 
   preload() {
@@ -17,14 +18,13 @@ export default class Game extends Phaser.Scene {
   }
 
   create() {
-    
     this.color = Phaser.Display.Color.RandomRGB().color;
- 
+
     this.cameras.main.setBackgroundColor(this.color);
 
     this.pelota = this.physics.add
       .image(400, 100, "pelota")
-      .setVelocity(50, 100)
+      .setVelocity(25, 50)
       .setBounce(1, 1)
       .setScale(0.07)
       .setCollideWorldBounds(true);
@@ -32,7 +32,9 @@ export default class Game extends Phaser.Scene {
     this.jugador = this.physics.add
       .image(400, 550, "jugador")
       .setScale(0.3)
+      .setImmovable()
       .setCollideWorldBounds(true);
+    this.jugador.body.allowGravity = false;
 
     this.physics.add.collider(
       this.jugador,
@@ -44,24 +46,48 @@ export default class Game extends Phaser.Scene {
 
     this.teclas = this.input.keyboard.createCursorKeys();
 
-    this.nivelTexto = this.add.text(15, 15, "Nivel " + this.nivel, {
-      fontSize: "15px",
+    this.nivelTexto = this.add.text(700, 15, "Nivel " + this.nivel, {
+      fontSize: "20px",
+    });
+
+    this.cantidadPuntosTexto = this.add.text(15, 15, "Puntos: " + this.puntos, {
+      fontSize: "20px",
       fill: "#FFFFFF",
     });
 
-    this.cantidadPuntosTexto = this.add.text(15, 30, "Puntos:" + this.puntos, {
-      fontSize: "15px",
+    this.cantidadVidasTexto = this.add.text(15, 40, "Vidas: " + this.vidas, {
+      fontSize: "20px",
       fill: "#FFFFFF",
     });
   }
 
   update() {
     if (this.teclas.left.isDown) {
-      this.jugador.setVelocityX(-300);
+      this.jugador.setVelocityX(-400);
     } else if (this.teclas.right.isDown) {
-      this.jugador.setVelocityX(300);
+      this.jugador.setVelocityX(400);
     } else {
       this.jugador.setVelocityX(0);
+    }
+
+    if (this.pelota.y > 550 && !this.toqueBorde) {
+      this.perderVida();
+    }
+
+    if (this.pelota.y <= 550) {
+      this.toqueBorde = false;
+    }
+
+    if (this.nivel == 20) {
+      this.ganarJuego();
+
+      this.scene.pause("game");
+    }
+
+    if (this.vidas == 0) {
+      this.perderJuego();
+
+      this.scene.pause("game");
     }
   }
 
@@ -70,11 +96,17 @@ export default class Game extends Phaser.Scene {
     this.choques++;
 
     // console.log("Puntos:" + this.puntos)
-    this.cantidadPuntosTexto.setText("Puntos:" + this.puntos);
+    this.cantidadPuntosTexto.setText("Puntos: " + this.puntos);
 
     if (this.choques >= 3) {
       this.pasarNivel();
     }
+  }
+
+  perderVida() {
+    this.toqueBorde = true;
+    this.vidas--;
+    this.cantidadVidasTexto.setText("Vidas: " + this.vidas);
   }
 
   pasarNivel() {
@@ -93,10 +125,10 @@ export default class Game extends Phaser.Scene {
   }
 
   agregarObstaculo() {
-    const obstaculoX = Phaser.Math.Between(50, 750); 
-    const obstaculoY = Phaser.Math.Between(200, 400); 
-    const ancho = Phaser.Math.Between(20, 70); 
-    const alto = 25; 
+    const obstaculoX = Phaser.Math.Between(50, 750);
+    const obstaculoY = Phaser.Math.Between(150, 400);
+    const ancho = Phaser.Math.Between(20, 70);
+    const alto = 25;
 
     this.obstaculos = this.physics.add.staticGroup();
 
@@ -107,5 +139,19 @@ export default class Game extends Phaser.Scene {
       .refreshBody();
 
     this.physics.add.collider(this.pelota, this.obstaculos);
+  }
+
+  ganarJuego() {
+    this.cantidadPuntosTexto = this.add.text(235, 200, "¡GANASTE!", {
+      fontSize: "60px",
+      fill: "#FFFFFF",
+    });
+  }
+
+  perderJuego() {
+    this.cantidadPuntosTexto = this.add.text(235, 200, "PERDISTE :(", {
+      fontSize: "60px",
+      fill: "#FFFFFF",
+    });
   }
 }
